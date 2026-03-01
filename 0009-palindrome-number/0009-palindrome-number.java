25871b1bea842f8854235edc6e02c5a3603eb9eb
Krishna Chauhan <chauhankb264@gmail.com>
2026-03-02 00:01:39 +0530

    Time: 5 ms (77.82%), Space: 45.9 MB (73.55%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int rev=0;
+        int orignal=x;
+        while(x!=0){
+                int rem=x%10;
+                rev=rev*10 + rem;
+                x=x/10;
+        }
+
+        if(orignal==rev){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+
+        
+    }
+}
