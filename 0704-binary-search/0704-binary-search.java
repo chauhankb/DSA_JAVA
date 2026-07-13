57f8e995e950a5f67da0e8d6700dd7eea46ceb98
Krishna Chauhan <chauhankb264@gmail.com>
2026-07-14 00:41:10 +0530

    Time: 0 ms (100%), Space: 48.4 MB (50.2%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -3,19 +3,21 @@ class Solution {
         int l=0;
         int r=nums.length-1;
 
-        while(l<=r){
-            int mid=(l+r)/2;
+        while(r>=l){
+            int mid=l+(r-l)/2;
 
-            if(nums[mid]==target) return mid;
-
-            else if(nums[mid]>target){
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[mid]>target){
                 r=mid-1;
             }
-            else{
+            if(nums[mid]<target){
                 l=mid+1;
             }
         }
 
         return -1;
+
     }
 }
